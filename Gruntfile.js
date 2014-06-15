a632ccd87d86e3657b10aed539f5c70f06dfa57a
Josh Justice <josh@need-bee.com>
2014-06-15 15:14:01 -0400

    adds requirejs combination and minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,34 @@ module.exports = function(grunt) {
       }
     },
 
+    requirejs: {
+      dist: {
+        options: {
+          mainConfigFile: 'js/main.js',
+          name: '../js/main',
+          out: 'js/main-built.js'
+        }
+      }
+    },
+
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
+      },
+      requirejs: {
+        files: 'js/main.js',
+        tasks: ['requirejs']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-requirejs');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass','requirejs']);
   grunt.registerTask('default', ['build','watch']);
 }

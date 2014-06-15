871ba44c9f96f94719ed9a65e9ee371b289d3233
Josh Justice <josh@need-bee.com>
2014-06-14 21:10:22 -0400

    switches to using require.js instead of direct script loading

---
+++ js/main.js
@@ -0,0 +1,19 @@
+require.config({
+	baseUrl: '../bower_components',
+	paths: {
+		'jquery': 'jquery/dist/jquery.min',
+		'foundation': 'foundation/js/foundation/foundation',
+		'foundation.topbar': 'foundation/js/foundation/foundation.topbar'
+	},
+	shim: {
+		'foundation.topbar': {
+			deps: ['foundation']
+		},
+		'foundation': {
+			deps: ['jquery']
+		}
+	}
+});
+require(['foundation.topbar'], function(){
+	$(document).foundation();
+});
